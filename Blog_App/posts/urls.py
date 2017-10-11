0ef34e3fc25c8c33491021d97c85c18816a6d69d
jebin007 <jevinsubedi@gmail.com>
2017-10-11 10:20:55 -0500

    Instance Update View

--- Blog_App/posts/urls.py
+++ Blog_App/posts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', "posts.views.post_list"),
     url(r'^create/$', "posts.views.post_create"),
     url(r'^(?P<id>\d+)/$', "posts.views.post_detail", name='detail'),
-    url(r'^update/$', "posts.views.post_update"),
+    url(r'^(?P<id>\d+)/edit/$', "posts.views.post_update", name='update'),
     url(r'^delete/$', "posts.views.post_delete"),
 ]
