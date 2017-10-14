ef5bb08d14cad8556800159e592aa9f6b3695040
jebin007 <jevinsubedi@gmail.com>
2017-10-14 12:51:53 -0500

    Basic User Permission For Create, Update and Delete Views

--- Blog_App/posts/views.py
+++ Blog_App/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from . models import Post
@@ -8,6 +8,8 @@ from urllib.parse import quote
 # Create your views here.
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -53,6 +55,8 @@ def post_list(request):
     return render(request, 'post_list.html', context)
 
 def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -70,6 +74,8 @@ def post_update(request, slug=None):
     return render(request, 'post_form.html', context)
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully Deleted!")
