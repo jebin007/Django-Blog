229daf0ccfb86b3966d253483ffd92cd0362c547
jebin007 <jevinsubedi@gmail.com>
2017-10-13 23:34:53 -0500

    SlugField To Replace Id in URL

--- Blog_App/posts/models.py
+++ Blog_App/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 def upload_location(instance, filename):
@@ -9,6 +9,7 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location, null=True, blank=True, width_field='width_field', height_field='height_field')
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
@@ -16,11 +17,18 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        super(Post, self).save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.title) + "-" + str(self.id)
+            self.save() 
+
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("posts:detail", kwargs={"id": self.id})
+        return reverse("posts:detail", kwargs={"slug": self.slug})
+
 
     class Meta:
         ordering = ['-timestamp', '-updated']
--- Blog_App/posts/urls.py
+++ Blog_App/posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', "posts.views.post_list", name='list'),
     url(r'^create/$', "posts.views.post_create"),
-    url(r'^(?P<id>\d+)/$', "posts.views.post_detail", name='detail'),
-    url(r'^(?P<id>\d+)/edit/$', "posts.views.post_update", name='update'),
-    url(r'^(?P<id>\d+)/delete/$', "posts.views.post_delete"),
+    url(r'^(?P<slug>[\w-]+)/$', "posts.views.post_detail", name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit/$', "posts.views.post_update", name='update'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', "posts.views.post_delete"),
 ]
